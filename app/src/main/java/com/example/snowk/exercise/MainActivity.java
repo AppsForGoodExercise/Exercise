b1165efe03c688e235778f286f2e298721bfee35
Jennifer <jkim5@wpi.edu>
2018-04-10 23:38:27 -0400

    test

--- app/src/main/java/com/example/snowk/exercise/MainActivity.java
+++ app/src/main/java/com/example/snowk/exercise/MainActivity.java
@@ -1,51 +1,49 @@
 package com.example.snowk.exercise;
 
-import android.app.Notification;
-import android.app.NotificationManager;
-import android.graphics.BitmapFactory;
-import android.support.v4.app.NotificationCompat;
-import android.support.v4.app.NotificationManagerCompat;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.InputStream;
+
+
 
 public class MainActivity extends AppCompatActivity {
 
-    Button button;
+
+    //linking homepage to notification page
+public Button buttonExercise;
+
+public void notiButton(){
+    buttonExercise = (Button)findViewById(R.id.dateButton);
+    buttonExercise.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Intent noti = new Intent(MainActivity.this,notificationActivity.class);
+            startActivity(noti);
+
+        }
+    });
+
+}
+
 
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
+
+
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //notification code
-        button = (Button) findViewById (R.id.notifyButton);
-        button.setOnClickListener(new View.OnClickListener() {
 
-            public void onClick(View view){
-                NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(MainActivity.this)
-                        .setSmallIcon(android.R.drawable.stat_notify_error)
-                        .setLargeIcon(BitmapFactory.decodeResource(getResources(),R.mipmap.ic_launcher))
-                        .setContentTitle("ExerTime")
-                        .setContentText("Time to exercise...");
-                notificationBuilder.setDefaults(
-                        Notification.DEFAULT_SOUND |
-                        Notification.DEFAULT_LIGHTS |
-                        Notification.DEFAULT_VIBRATE );
-                NotificationManagerCompat notificationManager = NotificationManagerCompat.from(MainActivity.this);
-                notificationManager.notify(1, notificationBuilder.build());
-            }
-        });
 
 
         InputStream is = (InputStream) getResources().openRawResource(R.raw.exerciselist);
