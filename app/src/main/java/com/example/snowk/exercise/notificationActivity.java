f6ee47c4c544d3eefbdb4e72e4e8432981bc244f
Jennifer <jkim5@wpi.edu>
2018-04-12 17:12:24 -0400

    deleted unused class and some extra space

--- app/src/main/java/com/example/snowk/exercise/notificationActivity.java
+++ app/src/main/java/com/example/snowk/exercise/notificationActivity.java
@@ -1,5 +1,9 @@
 package com.example.snowk.exercise;
 import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
 import android.graphics.BitmapFactory;
 import android.support.v4.app.NotificationCompat;
 import android.support.v4.app.NotificationManagerCompat;
@@ -7,6 +11,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RemoteViews;
 
 /**
  * Created by Jennifer on 4/9/2018.
@@ -21,7 +26,6 @@ public class notificationActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.notification);
 
-        Bundle bundle = getIntent().getExtras();
         button = (Button) findViewById(R.id.notifyButton);
 
         button.setOnClickListener(new View.OnClickListener() {
@@ -32,13 +36,14 @@ public class notificationActivity extends AppCompatActivity {
                         .setLargeIcon(BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher))
                         .setContentTitle("ExerTime")
                         .setContentText("Time to exercise!");
-                notificationBuilder.setDefaults(
-                        Notification.DEFAULT_SOUND | Notification.DEFAULT_LIGHTS | Notification.DEFAULT_VIBRATE);
+                notificationBuilder.setDefaults(Notification.DEFAULT_SOUND | Notification.DEFAULT_LIGHTS | Notification.DEFAULT_VIBRATE);
                 NotificationManagerCompat notificationManager = NotificationManagerCompat.from(notificationActivity.this);
                 notificationManager.notify(1, notificationBuilder.build());
 
             }
         });
+
+
     }
 }
 
