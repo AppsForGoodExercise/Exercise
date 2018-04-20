dc5afeb20e32594d17d6981e8814bb5d1ff52efd
Jennifer <jkim5@wpi.edu>
2018-04-20 13:35:15 -0400

    java files

---
+++ app/src/main/java/com/example/snowk/exercise/profile.java
@@ -0,0 +1,16 @@
+package com.example.snowk.exercise;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class profile extends AppCompatActivity {
+Button button;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);  
+
+        button = (Button) findViewById(R.id.profileButton);
+    }
+}
