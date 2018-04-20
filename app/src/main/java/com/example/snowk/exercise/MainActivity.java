88eca3d0185baca6d0021b75fcd2f1ea6cb3a0ff
Robert Clark <redmen239@gmail.com>
2018-04-20 13:54:13 -0400

    Merge branch 'master' of https://github.com/AppsForGoodExercise/Exercise
    # Conflicts:
    #	app/src/main/java/com/example/snowk/exercise/MainActivity.java

--- app/src/main/java/com/example/snowk/exercise/MainActivity.java
+++ app/src/main/java/com/example/snowk/exercise/MainActivity.java
@@ -3,7 +3,6 @@ package com.example.snowk.exercise;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.format.DateFormat;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -12,10 +11,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.Locale;
+
 
 
 public class MainActivity extends AppCompatActivity {
@@ -25,10 +21,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-        //Getting events will be here soon but we don't ahve google calendar access yet but wehn we do wei will be good.
-        ArrayList<Event> listofevents = new ArrayList<Event>();
-        //Generator of Exercise Master List
+        //Robert's Code
         InputStream is = (InputStream) getResources().openRawResource(R.raw.exerciselist);
         BufferedReader reader = new BufferedReader(new InputStreamReader(is));
 
@@ -54,20 +47,7 @@ public class MainActivity extends AppCompatActivity {
                 notificationpopup();
             }
         });
-        //Cosntructing A Day
-        Date d = new Date();
-        String date = new SimpleDateFormat("ddMMyyyy", Locale.getDefault()).format(new Date());
-        int dateinteger = Integer.parseInt(date);
 
-        Day day = new Day(dateinteger, listofevents, masterlists);
-
-
-    }
-
-
-    public void scheduleexercises(){
-
-    }
 
 
         //profile button
@@ -92,11 +72,13 @@ public class MainActivity extends AppCompatActivity {
        //next exercise button
        Button nextEx = findViewById(R.id.dateButton);
        nextEx.setOnClickListener(new View.OnClickListener() {
+
+
            @Override
            public void onClick(View view) {
                nextExercise();
            }
-       });
+       });}
 
         //Button scorebut = findViewById(R.id.scoreTitle);
       //  scorebut.setOnClickListener(new View.OnClickListener())
